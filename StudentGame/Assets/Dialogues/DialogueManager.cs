8e6cd8cb6f2834ec8e7057c263b60551fcc07b28
nicosare <dgalkin7@gmail.com>
2021-11-27 18:29:17 +0500

    Добавили файлы игры, реализована функция диалогов

---
+++ StudentGame/Assets/Dialogues/DialogueManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    public Animator animator;
+    private bool canContinue = true;
+    private Queue<string> sentences;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("isOne", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentences();
+    }
+
+    public void DisplayNextSentences()
+    {
+        if (canContinue)
+        {
+            if (sentences.Count == 0)
+            {
+                EndDialogue();
+                return;
+            }
+
+            string sentence = sentences.Dequeue();
+            StartCoroutine(TypeSentence(sentence));
+        }
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        canContinue = false;
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+        canContinue = true;
+    }
+
+    public void EndDialogue()
+    {
+
+        animator.SetBool("isOne", false);
+    }
+}
